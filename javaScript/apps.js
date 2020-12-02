c7fcba4a9aeabebbd2d26abb8c331440b932ebc7
alexgz <alexinc19@gmail.com>
2020-12-01 23:06:26 -0800

    Added event listners to the buttons

--- javaScript/apps.js
+++ javaScript/apps.js
@@ -1,1 +1,17 @@
 console.log("It's alive");
+
+$(".sleep").on("click", function () {
+    console.log("Dino is going to sleep");
+});
+
+$(".feed").on("click", function () {
+    console.log("Dino is eating");
+});
+
+$(".play").on("click", function () {
+    console.log("Dino is playing");
+});
+
+const feeding = function feeding() {
+
+}
