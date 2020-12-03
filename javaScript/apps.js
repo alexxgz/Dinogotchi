7ce6785ba3b0e3cc827225ab4d241eab7232950e
alexgz <alexinc19@gmail.com>
2020-12-02 17:04:13 -0800

    Added hide and show functions to my buttons

--- javaScript/apps.js
+++ javaScript/apps.js
@@ -11,9 +11,30 @@ $(".feed").on("click", function () {
 $(".play").on("click", function () {
     console.log("Dino is playing");
 });
+
 $(".start-game").on("click", function () {
     console.log("The game has begun");
 });
 
 
+$(".start-game").on("click", function () {
+    $(".identity").show();
+});
+
+$(".start-game").on("click", function () {
+    $(".controls").show();
+});
+
+$(".start-game").click(function () {
+    $(".instructions").hide();
+});
+$(".start-game").click(function () {
+    $(".input-text").hide();
+});
+$(".start-game").click(function () {
+    $(".dino-egg").hide();
+});
+$(".start-game").click(function () {
+    $(".start-game").hide();
+});
 
